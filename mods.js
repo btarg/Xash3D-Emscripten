0c2f84f9ae9855a7d4dc8692e8a11536fac6e1c0
iCrazyBlaze <iCrazyBlaze@users.noreply.github.com>
2019-10-23 17:05:06 +0100

    :red_circle: Updated to latest build
    I haven't really touched this project for a long time. Don't expect active development

--- mods.js
+++ mods.js
@@ -1,13 +1,20 @@
 zipMods = [
-['hldm.zip','HLDM (62 MB)', 64668974],
-['uplink.zip', 'Uplink (44 MB)', 45496413],
-['uplink-nohc.zip', 'Uplink without HC (29 MB)', 30567963],
-['hc.zip', 'Hazard Course (22 MB)', 22766605],
-['dayone.zip', 'Day One (75 MB)', 78804629]
+['hldm.zip','HLDM (64M)', 64654514],
+['uplink.zip', 'Uplink (30M)', 30578171],
+['hc.zip', 'Hazard Course (23M)', 23391374],
+['dayone.zip', 'Day One (78M)', 78804629]
+];
+
+aczMods = [
+['hldm-cache.html','HLDM (64M)', 64654514],
+['hldm-cache-split.html','HLDM Split (64M)', 64654514],
+['uplink-cache.html', 'Uplink (30M)', 30578171],
+['hc-cache.html', 'Hazard Course (23M)', 23391374]
 ];
 
 pkgMods = [
-['hldm.js','HLDM (82 MB)'],
-['uplink.js', 'Uplink (61 MB)']
+['hldm.js','HLDM (85M)'],
+['uplink.js', 'Uplink (45M)'],
+['hc.js', 'Hazard Course (33M)']
 ];
 
